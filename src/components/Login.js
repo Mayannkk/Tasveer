913d826e768e6026eeaccda3b28e9a49d36e2696
mayank <mayank.hiranyawal1999@gmail.com>
2021-12-28 20:29:17 +0530

    created backend schema and created routing

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import FcGoogle from "react-icons/fc";
+// import shareVideo from "../Assets/share";
+// import logo from "../Assets/logo";
+
+const Login = () => {
+  return <div>login</div>;
+};
+
+export default Login;
